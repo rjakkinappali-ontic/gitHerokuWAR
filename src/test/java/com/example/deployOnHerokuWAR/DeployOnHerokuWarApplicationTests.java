e90d08bca4417cc1478582d7b98de456e10628be
rjakkinappali-ontic <rjakkinappali@ontic.co>
2022-03-14 17:21:31 +0530

    first commit

---
+++ src/test/java/com/example/deployOnHerokuWAR/DeployOnHerokuWarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.deployOnHerokuWAR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeployOnHerokuWarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
